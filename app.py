e1a3b144e70f4058e7a2c11165f229f31cd37a2f
scarlett-palavecinos <scarlett.palavecinos@soymas.cl>
2023-04-20 15:57:32 -0400

    tarea terminada

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_templates
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return render_templates(´index.html´)
+    return render_template('index.html')
