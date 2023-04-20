bcaf915bd99beb05aa57b901c576ae936a6b13aa
scarlett-palavecinos <scarlett.palavecinos@soymas.cl>
2023-04-20 15:49:06 -0400

    tarea terminada

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, render_templates
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_templates(´index.html´)
